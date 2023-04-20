305fc86f112bf38170a079fffac9ad579424edfa
Quinn Lord <quinndolin404@gmail.com>
2023-04-20 14:54:59 +0100

    add ordering and sorting by

--- src/components/Reviews/ReviewsPage.jsx
+++ src/components/Reviews/ReviewsPage.jsx
@@ -5,52 +5,52 @@ import toast from "react-hot-toast";
 import * as api from "../../api";
 
 const ReviewsPage = () => {
+  // Loading states
   const [areReviewsLoading, setAreReviewsLoading] = useState(true);
+  const [areSelectableCategoriesLoading, setAreSelectableCategoriesLoading] =
+    useState(true);
+
+  // Fetched state to display
   const [reviews, setReviews] = useState([]);
-  const [areCategoriesLoading, setAreCategoriesLoading] = useState(true);
-  const [categories, setCategories] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [category, setCategory] = useState(
-    searchParams.get("category") || "all"
-  );
-  const previousCategory = useRef("all");
-  const previousReviews = useRef([]);
+  const [selectableCategories, setSelectableCategories] = useState();
+  const [searchParams, setSearchParams] = useSearchParams({
+    order: "desc",
+  });
+
+  // References to step back to if an input fails
   const previousSearchParams = useRef();
 
   useEffect(() => {
     setAreReviewsLoading(true);
-    const categoryParam =
-      searchParams.get("category") !== "all"
-        ? { category: searchParams.get("category") }
-        : undefined;
+    api.fetchCategories().then((categories) => {
+      setSelectableCategories(categories);
+      setAreSelectableCategoriesLoading(false);
+    });
     api
-      .fetchReviews(categoryParam)
+      .fetchReviews(searchParams)
       .then((reviews) => {
-        previousReviews.current = reviews;
-        previousCategory.current = category;
-        previousSearchParams.current = searchParams;
         setReviews(reviews);
         setAreReviewsLoading(false);
       })
       .catch((error) => {
-        setCategory(previousCategory.current);
-        setReviews(previousReviews.current);
+        toast.error("Failed to load reviews, please try again later!");
         setSearchParams(previousSearchParams.current);
-        toast.error("Changing category failed! Please try again later.");
       });
-    api.fetchCategories().then((categories) => {
-      setCategories(categories);
-      setAreCategoriesLoading(false);
-    });
-  }, [searchParams, category, setSearchParams]);
+  }, [searchParams]);
 
-  const handleChangeCategory = (event) => {
-    setCategory(event.target.value);
-    setSearchParams((currentSearchParams) => {
-      const newParams = new URLSearchParams(currentSearchParams);
-      newParams.set("category", event.target.value);
-      return newParams;
-    });
+  const handleChangeSearchParam = (param) => {
+    previousSearchParams.current = searchParams;
+    return (event) => {
+      setSearchParams((currentSearchParams) => {
+        const newParams = new URLSearchParams(currentSearchParams);
+        if (!(param === "category" && event.target.value === "all")) {
+          newParams.set(param, event.target.value);
+          return newParams;
+        }
+        newParams.delete("category");
+        return newParams;
+      });
+    };
   };
 
   return (
@@ -58,34 +58,69 @@ const ReviewsPage = () => {
       <div className="flex flex-col items-center gap-1">
         <h1>
           <b>{`${
-            category.slice(0, 1).toUpperCase() + category.slice(1)
+            searchParams.has("category")
+              ? searchParams
+                  .get("category")
+                  .replaceAll("-", " ")
+                  .split(" ")
+                  .map((word) => {
+                    return word.slice(0, 1).toUpperCase() + word.slice(1);
+                  })
+                  .join(" ")
+              : "All"
           } Reviews`}</b>
         </h1>
-        <span className="flex gap-2">
-          {!areCategoriesLoading && (
-            <>
-              <label htmlFor="review category">Choose a category:</label>
-              <select
-                onChange={handleChangeCategory}
-                value={category}
-                className="text-center"
-                name="review category"
-              >
-                <option value="all">{"all"}</option>
-                {categories.map((category) => {
-                  return (
-                    <option key={category.slug} value={category.slug}>
-                      {category.slug}
-                    </option>
-                  );
-                })}
-              </select>
-            </>
-          )}
-          {!!areCategoriesLoading && <p>Loading...</p>}
-        </span>
+        <div className="flex flex-col items-end gap-1">
+          <span className="flex gap-1">
+            {!areSelectableCategoriesLoading && (
+              <>
+                <label htmlFor="review category">Choose a category:</label>
+                <select
+                  onChange={handleChangeSearchParam("category")}
+                  value={searchParams.get("category")}
+                  className="text-center"
+                  name="review category"
+                >
+                  <option value="all">{"all"}</option>
+                  {selectableCategories.map((category) => {
+                    return (
+                      <option key={category.slug} value={category.slug}>
+                        {category.slug}
+                      </option>
+                    );
+                  })}
+                </select>
+              </>
+            )}
+            {!!areSelectableCategoriesLoading && <p>Loading...</p>}
+          </span>
+          <span className="flex gap-1">
+            <label htmlFor="order by">Order:</label>
+            <select
+              onChange={handleChangeSearchParam("order")}
+              value={searchParams.get("order")}
+              className="text-center"
+              name="order by"
+            >
+              <option value="asc">{"ascending"}</option>
+              <option value="desc">{"descending"}</option>
+            </select>
+          </span>
+          <span className="flex gap-1">
+            <label htmlFor="sort by">Sort By:</label>
+            <select
+              onChange={handleChangeSearchParam("sort_by")}
+              value={searchParams.get("sort_by")}
+              className="text-center"
+              name="sort by"
+            >
+              <option value="comment_count">{"comments"}</option>
+              <option value="created_at">{"time"}</option>
+              <option value="votes">{"votes"}</option>
+            </select>
+          </span>
+        </div>
       </div>
-
       {!areReviewsLoading &&
         reviews.map((review, index) => {
           return <ReviewCard key={index} review={review} />;
